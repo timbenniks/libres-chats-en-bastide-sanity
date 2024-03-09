515c575997b63d54e3ceebe06d6fd27e91655e68
Tim Benniks <tbenniks@gmail.com>
2024-03-09 19:29:44 +0100

    trying to set up visual editing

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -3,7 +3,10 @@ import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
 import { frFRLocale } from '@sanity/locale-fr-fr'
+import { presentationTool } from 'sanity/presentation'
+import { locate } from './locate'
 
+const SANITY_STUDIO_PREVIEW_URL = process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000'
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +15,7 @@ export default defineConfig({
   projectId: '9naj3yz0',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), frFRLocale()],
+  plugins: [presentationTool({ previewUrl: SANITY_STUDIO_PREVIEW_URL, locate }), structureTool(), visionTool(), frFRLocale()],
 
   schema: {
     types: schemaTypes,
--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxtjs/sanity'],
   sanity: {
     projectId: '9naj3yz0',
     dataset: 'production',
-    visualEditing: undefined
+    visualEditing: {
+      studioUrl: "http://localhost:3333",
+      token: "sk2zQ7v5kmSI3xErRZVyDrjJU4DkavD26SD8CY0UwBWePTd1eq33yLABVr4WyRTnt2PuGgL3d0WyEF06EaYyvtflbMoL4FXstN4aH71YpZtPTVzsjiJbYPX9XWpdFWYE1RMXSuBo6LOXYdP8l3mnY88cReB2ABGbr5sSXrDa9MzUXVlJJqLY"
+    }
   }
 })
