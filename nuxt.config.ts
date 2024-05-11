ee2306c0ceb07b15d7921b1176955d52766a1a01
Tim Benniks <tbenniks@gmail.com>
2024-05-11 10:18:00 +0200

    crossOriginPrefetch: false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
   experimental: {
-    crossOriginPrefetch: true
+    crossOriginPrefetch: false
   },
   modules: [
     "@nuxtjs/sanity",
